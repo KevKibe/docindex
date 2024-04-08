fa6472f7e4df1d0f1f1dc617a46124e18271cb16
KevKibe <kchegz234@gmail.com>
2024-04-08 20:59:08 +0300

    add: create indx cli function

---
+++ src/_google/create_index.py
@@ -0,0 +1,15 @@
+from .docindex import GooglePineconeIndexer
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Creates an Index on Pinecone.")
+    parser.add_argument("--pinecone_api_key", type=str, help="Pinecone API key")
+    parser.add_argument("--index_name", type=str, help="Name of the Pinecone index")
+    parser.add_argument("--google_api_key", type=str, help="Google API key")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pinecone_indexer = GooglePineconeIndexer(args.index_name, args.pinecone_api_key, args.google_api_key)
+    pinecone_indexer.create_index()
