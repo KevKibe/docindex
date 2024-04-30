d560cbeea359c81c96589811f412e7426c9ae27b
KevKibe <kchegz234@gmail.com>
2024-04-30 18:36:06 +0300

    fix: vectorstore parameter in test_04_retrieve_and_generate method

--- src/tests/googleindex_test.py
+++ src/tests/googleindex_test.py
@@ -63,9 +63,9 @@ class TestGooglePineconeIndexer(unittest.TestCase):
         """
         Test initializing the vector store and assert its type.
         """
-        vectorstore = self.indexer.initialize_vectorstore(self.index_name)
+        vector_store = self.indexer.initialize_vectorstore(self.index_name)
         response = self.indexer.retrieve_and_generate(query = "give a short summary of the introduction",
-                                                      vectorstore= vectorstore
+                                                      vectorstore= vector_store
                                                       )
         print(response)
         self.assertIsNotNone(response, "The retriever response should not be None.")
