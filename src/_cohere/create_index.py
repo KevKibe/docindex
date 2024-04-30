70d98003dcc31e8b9b139648005449290d2eeebe
KevKibe <kchegz234@gmail.com>
2024-04-30 14:48:20 +0300

    add indexing using cohere embeddings module and cli usage

---
+++ src/_cohere/create_index.py
@@ -0,0 +1,14 @@
+from .doc_index import CoherePineconeIndexer
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Creates an Index on Pinecone.")
+    parser.add_argument("--pinecone_api_key", type=str, help="Pinecone API key")
+    parser.add_argument("--index_name", type=str, help="Name of the Pinecone index")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    pinecone_indexer = CoherePineconeIndexer(args.index_name, args.pinecone_api_key)
+    pinecone_indexer.create_index()
